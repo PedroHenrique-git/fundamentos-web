e5d3944b3a1d3d29ab8e7887f9f1d4dbb554e0e0
PedroHenrique-git <pedromf34@outlook.com>
2022-02-03 22:18:01 -0300

    adjustments

--- lab-06/assets/js/index.js
+++ lab-06/assets/js/index.js
@@ -9,31 +9,55 @@
         }
     };
 
+    const returnClassOfClassification = (classificationNumber) => {
+        const classificationClasses = {
+            '0': 'green',
+            '10': 'blue',
+            '12': 'yellow',
+            '14': 'orange',
+            '16': 'red',
+            '18': 'black'
+        };
+        
+        return classificationClasses[classificationNumber];
+    }
+
     const createFilmCards = async () => {
         const films = await fetchJson('https://rafaelescalfoni.github.io/desenv_web/filmes.json');
 
         const filmsCardsHtml = films.map((film) => (`
             <div class="film-wrap">
-                <h1>
-                    ${film.titulo} ${film.classificacao}
-                </h1>
+                <h3 class="title">
+                    ${film.titulo}
+                </h3>
                 <div class="film-image-wrap">
                     <img src="${film.figura}" alt="${film.titulo}" />
                 </div>
                 <div class="film-infos-wrap">
-                    <p>${film.resumo}</p>
+                    <p>
+                        Classificação indicativa:
+                        <span class="classification ${returnClassOfClassification(film.classificacao)}">
+                            ${film.classificacao}
+                        </span>
+                    </p>
+                    <p>
+                        ${film.resumo}
+                    </p>
                     <ul class="film-genres">
+                        <li>Genêros: </li>
                         ${film.generos.map((genre) => `<li>${genre}</li>`).join('')}
                     </ul>
                     <ul class="film-cats">
+                        <li>Elenco: </li>
                         ${film.elenco.map((cast) => `<li>${cast}</li>`).join('')}
                     </ul>
                     <ul class="film-opinions">
+                        <li>Opiniões: </li>
                         ${film.opinioes.map((opinion) => 
                             `
                             <li>
-                                Avaliação: ${opinion.rating}
-                                Avaliação descrição: ${opinion.descricao} 
+                                <p>Avaliação: ${opinion.rating}</p>
+                                <p>${opinion.descricao} </p>
                             </li>
                             `
                         ).join('')}
